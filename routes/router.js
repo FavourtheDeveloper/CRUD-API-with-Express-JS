77dea1382e081bfefbc6f6dd4b25e8c5ecdde876
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-29 02:19:13 +0100

    Added API link and route

--- routes/router.js
+++ routes/router.js
@@ -1,30 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const members = require('../api/members')
-const addNewMember = require('../controllers/newMember')
+const members = require("../api/members");
+const addNewMember = require("../controllers/newMember");
 
 // get all members
 router.get("/", (req, res) => {
-    res.render('index', {title: "Members", members})
-  });
+  res.render("index", { title: "Members", members });
+});
 
-// add new member  
-  
-  router.post("/", addNewMember);
+// add new member
 
-  // get a single member with the id
-  
-  router.get('/:id', (req, res) => {
-      const result = members.filter((member) => member.id == req.params.id);
-      res.send(result)
-  })
+router.post("/", addNewMember);
 
-  // get members api
+// get members api
 
-  router.get('/api', (req, res) => {
-    res.json(members);
-  })
-  
+router.get("/api", (req, res) => {
+  res.json(members);
+});
 
-  module.exports =  router;
+// get a single member with the id
+
+router.get("/:id", (req, res) => {
+  const result = members.filter((member) => member.id == req.params.id);
+  res.send(result);
+});
+
+module.exports = router;
